e0ac1711d5589dadf1f8339dbfffcff19efb2d81
Christopher Fields <chrisjr06@gmail.com>
2021-06-21 10:53:01 -0500

    fix verify request page to use callback url as link

--- src/server/pages/verify-request.js
+++ src/server/pages/verify-request.js
@@ -1,11 +1,15 @@
-import { h } from 'preact' // eslint-disable-line no-unused-vars
+import { h } from "preact" // eslint-disable-line no-unused-vars
 
-export default function verifyRequest ({ baseUrl }) {
+export default function verifyRequest({ baseUrl, callbackUrl }) {
   return (
-    <div className='verify-request'>
+    <div className="verify-request">
       <h1>Check your email</h1>
       <p>A sign in link has been sent to your email address.</p>
-      <p><a className='site' href={baseUrl}>{baseUrl.replace(/^https?:\/\//, '')}</a></p>
+      <p>
+        <a className="site" href={callbackUrl}>
+          {callbackUrl.replace(/^https?:\/\//, "")}
+        </a>
+      </p>
     </div>
   )
 }
